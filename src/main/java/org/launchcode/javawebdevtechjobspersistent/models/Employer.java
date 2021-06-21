a6d1a5bbd33d3c5c037499156841a794eeff72f3
Will Staton <will.staton@gmail.com>
2021-06-21 13:59:55 -0500

    finished part 2

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Cannot be blank")
+    @Size(min = 1, max = 255, message = "Must be between 1 and 255 characters")
+    private String location;
 
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
