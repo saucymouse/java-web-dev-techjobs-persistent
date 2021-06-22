35a6d230e12bdc60aa855ff702eeae34258675df
Will Staton <will.staton@gmail.com>
2021-06-21 23:25:38 -0500

    finished assignment

--- queries.sql
+++ queries.sql
@@ -1,7 +1,19 @@
 ## Part 1: Test it with SQL
+select column_name, data_type
+from information_schema.columns
+where table_schema = 'techjobs' and table_name = 'job';
 
 ## Part 2: Test it with SQL
+select location
+from employer
+where location = 'St. Louis City';
 
 ## Part 3: Test it with SQL
+drop table job;
 
 ## Part 4: Test it with SQL
+select distinct skill.name, skill.description
+from job_skills
+join job on job.id = job_skills.jobs_id
+join skill on  skill.id = job_skills.skills_id
+order by skill.name asc;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +14,11 @@ public class Employer extends AbstractEntity {
     @NotBlank(message = "Cannot be blank")
     @Size(min = 1, max = 255, message = "Must be between 1 and 255 characters")
     private String location;
+
+    @OneToMany
+    //TODO circle back on this annotation
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer() {
     }
